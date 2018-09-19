c602827238b3bfc3c4f60ae25e17c9b40cd85e39
Rafael León <rafa.leon190@gmail.com>
2018-09-19 11:11:20 -0600

    it works with python deep_learning done

--- l3_Rafa.R
+++ l3_Rafa.R
@@ -3,4 +3,24 @@ install.packages(c("keras", "tensorflow"))
 library(keras)
 library(tensorflow)
 
+data <- read.csv("data/train.csv")
+
+
+set.seed(123)
+
+split <- 70/100
+
+part <-  sample(1:nrow(data),split*nrow(data))
+train <- data[part,]
+test <-  data[-part,]
+
+
+# Convolutional Neural netowrk model
+## Data normalization and categorical scaling
+
+train  <- as.matrix(train)
+test <- as.matrix(test)
+
+train <- normalize(train[,2:785])
+test <- normalize(test[,2:785])
 
